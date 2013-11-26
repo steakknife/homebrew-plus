8df3ce23e9ea84034a1811d0f85c896d6792f055
Barry Allard <barry.allard@gmail.com>
2013-11-25 21:30:53 -0800

    [gnupg, gnupg2] ftp -> http

---
+++ libassuan.rb
@@ -0,0 +1,14 @@
+require 'formula'
+
+class Libassuan < Formula
+  homepage 'http://www.gnupg.org/related_software/libassuan/index.en.html'
+  url 'http://ftp.heanet.ie/mirrors/ftp.gnupg.org/gcrypt/libassuan/libassuan-2.1.1.tar.bz2'
+  sha1 '8bd3826de30651eb8f9b8673e2edff77cd70aca1'
+
+  depends_on 'libgpg-error'
+
+  def install
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make install"
+  end
+end
