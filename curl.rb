0d408f1acd4773828576106977b60244d084c52a
Barry Allard <barry.allard@gmail.com>
2013-11-29 11:09:31 -0800

    [curl] add ipv6, libidn, tls-srp

--- curl.rb
+++ curl.rb
@@ -14,12 +14,16 @@ class Curl < Formula
   option 'with-ssl', 'Build with Homebrew OpenSSL instead of the system version'
   option 'with-darwinssl', 'Build with Secure Transport for SSL support'
   option 'with-gssapi', 'Build with GSSAPI/Kerberos authentication support.'
+  option 'with-idn', 'Build with internationalized domain support.'
+  option 'with-tls-srp', 'Build with Transport Layer Security Secure Remote Password support.'
+  option 'with-ipv6', 'Build with IPv6 support.'
 
   depends_on 'pkg-config' => :build
   depends_on 'libmetalink' => :optional
   depends_on 'libssh2' if build.with? 'ssh'
   depends_on 'c-ares' if build.with? 'ares'
   depends_on 'openssl' if build.with? 'ssl'
+  depends_on 'libidn' if build.with? 'idn'
 
   def install
     args = %W[
@@ -34,6 +38,9 @@ class Curl < Formula
     args << "--with-ssl=#{Formula.factory("openssl").opt_prefix}" if build.with? 'ssl'
     args << "--with-darwinssl" if build.with? 'darwinssl'
     args << "--with-gssapi" if build.with? 'gssapi'
+    args << "--with-libidn=#{Formula.factory("libidn").opt_prefix}" if build.with? 'idn'
+    args << "--enable-tls-srp" if build.with? 'tls-srp'
+    args << "--enable-ipv6" if build.with? 'ipv6'
 
     system "./configure", *args
     system "make install"
